7e3b4b2f9453e42b45629568e4d5101bd86955ce
Austin Gregory <austingregory82@gmail.com>
2023-07-04 20:50:49 -0600

    Has Menu in home page, has bottom bar in home page, has hover capabilities in featured items section, has menu data

---
+++ src/data/menuData.js
@@ -0,0 +1,101 @@
+export const menuData = {
+    coffee_drinks: {
+        displayName: "Coffee Drinks",
+        items: [
+        {
+            name: "Pottsfield Potion",
+            description: "Pottsfield",
+            price: 6
+        },
+        {
+            name: "Snow in May",
+            description: "Lavender Latte with CBD",
+            price: 5,
+        },
+        {
+            name: "The Spider Queen",
+            description: "Cinnamon Apple Latte with four shots of Espresso",
+            price: 5,
+        }]
+    },
+    specialty_drinks: {
+        displayName: "Specialty Drinks",
+        items: [
+        {
+            name: "Pottsfield Potion",
+            description: "Head over the garden wall with this pumpkin mocha latte spiced with cinnamon",
+            price: 6
+        },
+        {
+            name: "Unlimited Void",
+            description: "Expand your domain with this dark chocolate mocha latte",
+            price: 6
+        },
+        {
+            name: "Running Out of Time",
+            description: "Get this rose latte with four shots of espresso for your paramour",
+            price: 6
+        },
+        {
+            name: "Freya's Foray",
+            description: "Defeat your enemies with this earl grey tea latte with a special mystery blend of syrups",
+            price: 6
+        },
+        {
+            name: "Snow in May",
+            description: "Lavender latte with CBD",
+            price: 5,
+        },
+        {
+            name: "The Spider Queen",
+            description: "Cinnamon apple latte with four shots of espresso",
+            price: 5,
+        }]
+    },
+    non_coffee_drinks: {
+        displayName: "Tea/Non Coffee Drinks",
+        items: [
+        {
+            name: "Earl Grey Sweatshirt",
+            description: "The description doesn't fit if not a synonym of menace",
+            price: 4
+        },
+        {
+            name: "Snow in May",
+            description: "Lavender Latte with CBD",
+            price: 5,
+        },
+        {
+            name: "The Spider Queen",
+            description: "Cinnamon Apple Latte with four shots of Espresso",
+            price: 5,
+        }]
+    },
+    food:  {
+        displayName: "Food",
+        items: [
+            {
+                name: "Grilled Cheese",
+                description: "It is grilled cheese, you already know what it is",
+                price: 6
+            },
+            {
+                name: "Breakfast Burrito",
+                description: "POTATOES AND BACON AND GREEN CHILE AND EGGS AND CHEESE",
+                price: 5,
+            },
+            {
+                name: "Good Soup",
+                description: "Not to be confused with bad soup",
+                price: 5,
+            },
+            {
+                name: "Potatoes and Molasses",
+                description: "Keep your stomach from rumbling!",
+                price: 5,
+            }  
+        ]
+    }
+    
+
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,18 +2,21 @@ import React from "react";
 import './home.css'
 import Grid from '@mui/material/Unstable_Grid2'
 import VengefulBeansPixelArt from "../VengefulBeansCoffeeShopPixelArt.png"
+import TheSpiderQueen from "../assets/TheSpiderQueen.png"
+import SnowInMay from "../assets/SnowInMay.png"
+import {menuData} from "../data/menuData.js"
+import {SocialIcon} from 'react-social-icons';
 
 const backgroundStyles = {
     width: "100%",
-    height: "100vh",
-    background: '#ffaec8',
+    height: "100%",
+    background: '#f4c3ce',
 };
 
 const imageStyle = {
     display: "block",
     marginLeft: "auto",
     marginRight: "auto",
-    maxWidth: "100vw",
     width: "100%",
     height: "100%",
     maxWidth: 788,
@@ -25,7 +28,6 @@ const scheduleStyles = {
     display: "block",
     marginLeft: "auto",
     marginRight: "auto",
-    maxWidth: "100vw",
     padding: "20px 0",
     width: "100%",
     height: "100%",
@@ -35,13 +37,162 @@ const scheduleStyles = {
     background: "#d69e6e",
     color: "#000000",
     fontSize: "1.5em",
-    fontFamily: "Century Gothic,CenturyGothic,AppleGothic,sans-serif",
+    fontFamily: "Bradley Hand,cursive",
     textAlign: "center",
     whiteSpace: "pre-line",
+};
+
+const featuredItemsStyles = {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    padding: "20px 0",
+    width: "100%",
+    height: "100%",
+    maxWidth: 788,
+    maxHeight: 424,
+    border: "1px solid black",
+    background: "#ffaec8",
+    color: "#000000",
+    fontSize: "1.5em",
+    fontFamily: "Bradley Hand,cursive",
+    textAlign: "center",
+    whiteSpace: "pre-line",
+};
+
+const menuStyles = {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    padding: "20px 0",
+    width: "100%",
+    height: "100%",
+    maxWidth: 788,
+    maxHeight: "100vh",
+    border: "1px solid black",
+    background: "#d69e6e",
+    color: "#000000",
+    fontSize: "1.5em",
+    fontFamily: "Bradley Hand,cursive",
+    textAlign: "center",
+    whiteSpace: "pre-line",
+};
+
+const menuCategoryStyles = {
+    padding: "10px 0",
+    fontSize: "1em",
+    fontFamily: "Bradley Hand,cursive",
+    textAlign: "center",
+    whiteSpace: "pre-line",
+    background: "#ffaec8",
+};
+
+const menuItemStyles = {
+    fontSize: "0.75em",
+    fontFamily: "Bradley Hand,cursive",
+    textAlign: "center",
+    whiteSpace: "pre-line",
+};
+
+const menuItemDescriptionStyles = {
+    fontSize: "0.5em",
+    fontFamily: "Bradley Hand,cursive",
+    textAlign: "center",
+    whiteSpace: "pre-line",
+};
+
+const bottomBarStyles = {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    padding: "5px 0",
+    width: "100%",
+    height: "100%",
+    maxWidth: 788,
+    maxHeight: 128,
+    border: "1px solid black",
+    background: "#ffaec8",
+    color: "#000000",
+    fontSize: "0.75em",
+    fontFamily: "Bradley Hand,cursive",
+    textAlign: "center",
+    whiteSpace: "pre-line",
+};
+
+const featuredItemsGridStyles = {
+    '&:hover': {
+        '& .image-overlay': {
+            display: 'block',
+        },
+    },
+};
+
+const menuGridStyles = {
+    padding: "10px 0"
+};
+
+const socialMediaIconStyles = {
+    marginRight: "5px",
+    marginLeft: "5px"
 }
 
-const schedule = "\nSchedule:\n\nMonday - Friday: 6am - 6pm\nSaturday - Sunday: 6am - 8pm\n\n"
-const location = "Location:\n\n722 Paramore St."
+const schedule = "Schedule:\n\nMonday - Friday: 6am - 6pm\nSaturday - Sunday: 6am - 8pm\n\n";
+const location = "Location:\n\n722 Paramore St.";
+const featuredItems = "Featured Items\n\n";
+const menu = "Menu\n\n"
+const bottomBar = "© 2023 Vengeful Beans Coffee\n\n"
+
+const getDescriptionFromName = (menuItemName, menuItemType) => {
+    const { items } = menuData[menuItemType]
+    const item = items.find((item) => item.name === menuItemName);
+    const itemString = item.name + "\n\n" + item.description
+    return item ? itemString : null;
+}
+
+const fillMenuItems = (menuCategory) => {
+    const {items} = menuData[menuCategory];
+    var menuItems = [];
+    if (items) {
+      items.forEach((item) => {
+        const menuItem = renderMenuItem(item.name, item.description, item.price);
+        menuItems.push(menuItem);
+      });
+    }
+    return menuItems;
+};
+
+const renderMenuItem = (menuItemName, menuItemDescription, menuItemPrice) => {
+    const menuItemString = menuItemName + ".........." + menuItemPrice;
+    return (
+        <Grid xs={6}>
+            <div style={menuItemStyles}>
+                {menuItemString}
+            </div>
+            <div style={menuItemDescriptionStyles}>
+                {menuItemDescription}
+            </div>
+        </Grid>
+    )
+}
+
+const renderMenuCategory = (menuCategoryName) => {
+    const {displayName} = menuData[menuCategoryName];
+    return (
+        <div style={menuCategoryStyles}>{displayName}</div>
+    )
+}
+
+const renderMenu = () => {
+    return Object.entries(menuData).map(([categoryName, categoryData]) => (
+      <React.Fragment key={categoryName}>
+        <div style={{ border: "1px solid black" }}></div>
+        {renderMenuCategory(categoryName)}
+        <Grid style={menuGridStyles} container spacing={4}>
+          {fillMenuItems(categoryName)}
+        </Grid>
+      </React.Fragment>
+    ));
+}
 
 const Home = () => {
     return (
@@ -51,6 +202,38 @@ const Home = () => {
             </div>
             <div style={scheduleStyles}>
                 {schedule+location}
+            </div>
+            <div style={featuredItemsStyles}>
+                {featuredItems}
+                <Grid container spacing={4}>
+                    <Grid xs={6}>
+                        <div className="image-container" style={featuredItemsGridStyles}>
+                            <img className="image" src={SnowInMay}></img>
+                            <div className="image-overlay">
+                                {getDescriptionFromName("Snow in May", "specialty_drinks")}
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid xs={6}>
+                        <div className="image-container" style={featuredItemsGridStyles}>
+                            <img className="image" src={TheSpiderQueen}></img>
+                            <div className="image-overlay">
+                                {getDescriptionFromName("The Spider Queen", "specialty_drinks")}
+                            </div>
+                        </div>
+                    </Grid>
+                </Grid>
+            </div>
+            <div style={menuStyles}>
+                {menu}
+                <div>{renderMenu()}</div>
+            </div>
+            <div style={bottomBarStyles}>
+                {bottomBar}
+                <SocialIcon style={socialMediaIconStyles} url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" network="instagram"/>
+                <SocialIcon style={socialMediaIconStyles} url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" network="facebook"/>
+                <SocialIcon style={socialMediaIconStyles} url="https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=fdae361784b94a2f" network="spotify"/>
+                
             </div>
         </div>
 
